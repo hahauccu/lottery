b2a779e3f36280c04fcc43ac138c6794216637a7
victor <zxc25852ad40@gmail.com>
2021-10-04 02:24:07 +0800

    deal with test file and add a winner number test

---
+++ tests/Unit/LotteryTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Services\LotteryService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+class LotteryTest extends TestCase
+{
+	use RefreshDatabase;
+	protected function setUp(): void
+	{ 
+		parent::setUp();
+		$this->artisan('migrate');
+    	$this->artisan('db:seed');
+	}
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_if_lootery_winner_number_is_currect()
+    {
+    	$defultSteps = 1; 
+    	$lotteryService = new LotteryService();
+    	$lotterySteps = $lotteryService->getLotterySteps($defultSteps);
+    	$looteryResult = $lotteryService->doLottery($defultSteps);
+    	//is winner number currect ?
+		$this->assertEquals($lotterySteps['prize_number'],count( $looteryResult) );
+    }
+
+}
